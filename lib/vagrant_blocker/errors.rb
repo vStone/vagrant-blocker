e515e3b2a2753e07f09581ebab06821ccf6b7a97
Jan Vansteenkiste <jan@vstone.eu>
2016-10-28 15:33:34 +0200

    Some documentation on the errors to make rubocop happy

--- lib/vagrant_blocker/errors.rb
+++ lib/vagrant_blocker/errors.rb
@@ -2,10 +2,12 @@
 module VagrantPlugins
   module Blocker
     module Errors
+      # The current box blocks a vm that is already running
       class BoxBlocksRunningVM < Vagrant::Errors::VagrantError
         error_namespace('vagrant_blocker.errors')
         error_key(:box_blocks_running_vm)
       end
+      # The box is blocked by a vm that is already running
       class BoxBlockedByRunningVM < Vagrant::Errors::VagrantError
         error_namespace('vagrant_blocker.errors')
         error_key(:box_blocked_by_running_vm)
